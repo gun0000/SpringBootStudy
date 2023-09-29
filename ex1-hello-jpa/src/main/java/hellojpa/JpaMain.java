65ff9a46adb221ba2055038c89e0a78eada19b74
김건우 <guncode4545@gmail.com>
2023-09-29 09:07:16 +0900

    jpa 트랜잭션

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,24 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+//엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
+        transaction.begin(); // [트랜잭션] 시작
+        em.persist(memberA);
+        em.persist(memberB);
+//여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
+//커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
+        transaction.commit(); // [트랜잭션] 커밋
+    }
+
+
+}
