91e396f987feec067bd8af10f5ba8643751e4d82
김건우 <guncode4545@gmail.com>
2023-09-30 19:35:52 +0900

    jpa 엔티티매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,6 +6,7 @@ public class Member {
     @Id
     private Long id;
     private String name;
+
     //Getter, Setter
 
     public Long getId() {
@@ -23,4 +24,50 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
+
+    /*
+        * 데이터베이스 스키마 자동 생성 - 속성
+    hibernate.hbm2ddl.auto
+    옵션 설명
+    create 기존테이블 삭제 후 다시 생성 (DROP + CREATE)
+    create-drop create와 같으나 종료시점에 테이블 DROP
+    update 변경분만 반영(운영DB에는 사용하면 안됨)
+    validate 엔티티와 테이블이 정상 매핑되었는지만 확인
+    none 사용하지 않음
+    *
+        * 데이터베이스 스키마 자동 생성 - 속성
+    hibernate.hbm2ddl.auto
+    옵션 설명
+    create 기존테이블 삭제 후 다시 생성 (DROP + CREATE)
+    create-drop create와 같으나 종료시점에 테이블 DROP
+    update 변경분만 반영(운영DB에는 사용하면 안됨)
+    validate 엔티티와 테이블이 정상 매핑되었는지만 확인
+    none 사용하지 않음
+    * */
+
+    /*
+    * @Id
+     private Long id;
+     @Column(name = "name")
+     private String username;
+     private Integer age;
+     @Enumerated(EnumType.STRING)
+     private RoleType roleType;
+     @Temporal(TemporalType.TIMESTAMP)
+     private Date createdDate;
+     @Temporal(TemporalType.TIMESTAMP)
+     private Date lastModifiedDate;
+     @Lob
+     private String description;
+     *
+     * 매핑 어노테이션 정리
+    hibernate.hbm2ddl.auto
+    어노테이션 설명
+    @Column 컬럼 매핑
+    @Temporal 날짜 타입 매핑
+    @Enumerated
+    enum 타입 매핑
+    @Lob BLOB, CLOB 매핑
+    @Transient 특정 필드를 컬럼에 매핑하지 않음(매핑 무시)
+    * */
 }
