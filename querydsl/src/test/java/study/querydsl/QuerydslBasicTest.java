18558106a0e86034b92fc2b21261bb496a854649
guncode4545 <guncode4545@gmail.com>
2023-10-15 23:04:00 +0900

    Q클래스 인스턴스를 사용하는 2가지 방법

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -12,6 +12,7 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*; //QMember qMember = QMember.member; //기본 인스턴스 사용
 
 @SpringBootTest
 @Transactional
@@ -46,6 +47,26 @@ public class QuerydslBasicTest {
                 .select(m)
                 .from(m)
                 .where(m.username.eq("member1"))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+
+/*
+    Q클래스 인스턴스를 사용하는 2가지 방법
+    QMember qMember = new QMember("m"); //별칭 직접 지정
+     참고: 같은 테이블을 조인해야 하는 경우가 아니면 기본 인스턴스를 사용하자
+    QMember qMember = QMember.member; //기본 인스턴스 사용
+    기본 인스턴스를 static import와 함께 사용
+*/
+    @Test
+    public void startQuerydsl3() {
+        //QMember qMember = QMember.member; //기본 인스턴스 사용 //기본 인스턴스를 static import와 함께 사용
+        //member1을 찾아라.
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
