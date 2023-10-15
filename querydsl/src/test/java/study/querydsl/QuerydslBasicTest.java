1f469ffca3f96fda4a3e9583d19ec900b05f07c8
guncode4545 <guncode4545@gmail.com>
2023-10-15 23:16:26 +0900

    검색 조건 쿼리

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -11,6 +11,8 @@ import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.*; //QMember qMember = QMember.member; //기본 인스턴스 사용
 
@@ -70,4 +72,48 @@ public class QuerydslBasicTest {
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    /*
+        검색 조건 쿼리
+        검색 조건은 .and() , . or() 를 메서드 체인으로 연결할 수 있다.
+        참고: select , from 을 selectFrom 으로 합칠 수 있음
+     */
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+    //AND 조건을 파라미터로 처리
+    @Test
+    public void searchAndParam() {
+        List<Member> result1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"), member.age.eq(10))
+                .fetch();
+        assertThat(result1.size()).isEqualTo(1);
+    }
+    //where() 에 파라미터로 검색조건을 추가하면 AND 조건이 추가됨
+    //이 경우 null 값은 무시 메서드 추출을 활용해서 동적 쿼리를 깔끔하게 만들 수 있음 뒤에서 설명
+    /*
+    member.username.eq("member1") // username = 'member1'
+    member.username.ne("member1") //username != 'member1'
+    member.username.eq("member1").not() // username != 'member1'
+    member.username.isNotNull() //이름이 is not null
+    member.age.in(10, 20) // age in (10,20)
+    member.age.notIn(10, 20) // age not in (10, 20)
+    member.age.between(10,30) //between 10, 30
+    member.age.goe(30) // age >= 30
+    member.age.gt(30) // age > 30
+    member.age.loe(30) // age <= 30
+    member.age.lt(30) // age < 30
+    member.username.like("member%") //like 검색
+    member.username.contains("member") // like ‘%member%’ 검색
+    member.username.startsWith("member") //like ‘member%’ 검색
+...
+     */
+
+
 }
