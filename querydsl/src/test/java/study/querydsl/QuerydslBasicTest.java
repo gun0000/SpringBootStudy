4a4446f4662813d7c8e6989b790f2734c4c427a8
guncode4545 <guncode4545@gmail.com>
2023-10-15 22:45:50 +0900

    JPQL vs Querydsl

---
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,52 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @PersistenceContext
+    EntityManager em;
+    JPAQueryFactory queryFactory;
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        //초기화
+        em.flush();
+        em.clear();
+    }
+    @Test
+    public void startQuerydsl2() {
+        //member1을 찾아라.
+        QMember m = new QMember("m");
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
