a03b2c17c174fab250c541ee98b8dc5e9696cd7b
guncode4545 <guncode4545@gmail.com>
2023-10-20 14:31:59 +0900

    기본 조인 , 세타 조인

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import java.util.List;
@@ -333,6 +334,64 @@ public class QuerydslBasicTest {
     .having(item.price.gt(1000)
      */
 
+
+    /*
+        조인 - 기본 조인
+        기본 조인
+        조인의 기본 문법은 첫 번째 파라미터에 조인 대상을 지정하고, 두 번째 파라미터에 별칭(alias)으로 사용할
+        Q 타입을 지정하면 된다.
+
+        join(조인 대상, 별칭으로 사용할 Q타입)
+     */
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() throws Exception {
+        QMember member = QMember.member;
+        QTeam team = QTeam.team;
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+        assertThat(result)
+                .extracting("username")//List<Member> 형태의 result에서 username 필드값만 추출
+                .containsExactly("member1", "member2");//추출된 username 값들이 정확히 "member1", "member2"인지 확인
+        //result        [Member(id=1, username=member1, age=10), Member(id=2, username=member2, age=20)]
+        //result.get(0) Member(id=1, username=member1, age=10)
+    /*
+        join() , innerJoin() : 내부 조인(inner join)
+        leftJoin() : left 외부 조인(left outer join)
+        rightJoin() : rigth 외부 조인(rigth outer join)
+        JPQL의 on 과 성능 최적화를 위한 fetch 조인 제공 다음 on 절에서 설명
+     */
+    }
+
+
+    /**
+     * 세타 조인
+     * 연관관계가 없는 필드로 조인
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+    /*
+        from 절에 여러 엔티티를 선택해서 세타 조인
+        외부 조인 불가능 다음에 설명할 조인 on을 사용하면 외부 조인 가능
+    */
 
 
 }
