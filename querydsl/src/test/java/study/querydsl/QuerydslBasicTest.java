da8cf9dcbaf63aa6947954e62e1d2b2d4f9e0666
guncode4545 <guncode4545@gmail.com>
2023-10-31 15:07:56 +0900

    프로젝션과 결과 반환 - 기본

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -720,6 +720,43 @@ public class QuerydslBasicTest {
     }
 
 
+/*
+중급 문법
+프로젝션과 결과 반환 - 기본
+*/
+
+
+    /*
+        프로젝션: select 대상 지정
+     */
+    @Test
+    public void 반환타입하나지정(){
+        //String
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        //프로젝션 대상이 하나면 타입을 명확하게 지정할 수 있음
+    }
+    //프로젝션 대상이 둘 이상이면 튜플이나 DTO로 조회
+    //튜플 조회
+    //프로젝션 대상이 둘 이상일 때 사용
+    @Test
+    public void 반환타입여러개지정(){
+        //Tuple
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username=" + username);
+            System.out.println("age=" + age);
+        }
+    }
+    //Tuple은 다른 계층에서까지 쓰기는 안좋기 때문에 레파지토리 내에서 쓰거나 DTO방식을 쓰는게 좋다
+
 
 
 
